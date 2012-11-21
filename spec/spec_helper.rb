2e14338e0ec26ebaa469db754048d11c77cb878c
Andrew De Ponte <cyphactor@gmail.com>
2012-11-20 23:49:19 -0800

    Added basic rspec setup.
    I did this so that I could start using RSpec to develop this gem.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+end
